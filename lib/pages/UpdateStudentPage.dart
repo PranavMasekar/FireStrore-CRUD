c936c3f16b838a236bf407872bf6a3b89878a9e1
Pranav Masekar <83063593+PranavMasekar@users.noreply.github.com>
2021-10-24 16:59:07 +0530

    FireStore Connected

--- lib/pages/UpdateStudentPage.dart
+++ lib/pages/UpdateStudentPage.dart
@@ -6,8 +6,117 @@ class UpdateStudentPage extends StatefulWidget {
 }
 
 class _UpdateStudentPageState extends State<UpdateStudentPage> {
+  final _formKey = GlobalKey<FormState>();
+  updateUser() {}
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update Student'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  initialValue: 'Pranav',
+                  autofocus: false,
+                  onChanged: (value) => {},
+                  decoration: InputDecoration(
+                    labelText: 'Name: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  initialValue: 'PranavMasekar4@gmail.com',
+                  autofocus: false,
+                  onChanged: (value) => {},
+                  decoration: InputDecoration(
+                    labelText: 'Email:',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  initialValue: '21656566',
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, otherwise false.
+                        if (_formKey.currentState!.validate()) {
+                          updateUser();
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Update',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {},
+                      child: Text(
+                        'Reset',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/addstudent.dart
+++ lib/pages/addstudent.dart
@@ -6,8 +6,147 @@ class AddStudentPage extends StatefulWidget {
 }
 
 class _AddStudentPageState extends State<AddStudentPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  var name = "";
+  var email = "";
+  var password = "";
+
+  final namecontrl = TextEditingController();
+  final emailcontrl = TextEditingController();
+  final passcontrl = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    namecontrl.dispose();
+    emailcontrl.dispose();
+    passcontrl.dispose();
+    super.dispose();
+  }
+
+  clearText() {
+    namecontrl.clear();
+    emailcontrl.clear();
+    passcontrl.clear();
+  }
+
+  addUser() {
+    print('user added');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add New Student"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Name: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: namecontrl,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Email: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: emailcontrl,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                        TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: passcontrl,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, otherwise false.
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            name = namecontrl.text;
+                            email = emailcontrl.text;
+                            password = passcontrl.text;
+                            addUser();
+                            clearText();
+                          });
+                        }
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {clearText()},
+                      child: Text(
+                        'Reset',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
