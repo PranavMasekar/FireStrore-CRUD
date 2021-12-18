ab4b55fa25e3ac1137fb829ad5eacb9aa72b0f79
Pranav Masekar <83063593+PranavMasekar@users.noreply.github.com>
2021-12-18 19:42:17 +0530

    run karake bata to

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,74 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firestore_project/pages/VisitStore.dart';
-import 'package:firestore_project/pages/home.dart';
-import 'package:firestore_project/pages/login.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// ignore_for_file: avoid_print, non_constant_identifier_names, unused_field
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initilization = Firebase.initializeApp();
+class _MyAppState extends State<MyApp> {
+  String _status = '';
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void authenticate(String URL) async {
+    const callbackUrlScheme = "pranavapp.page.link";
+    try {
+      final result = await FlutterWebAuth.authenticate(
+          url: URL, callbackUrlScheme: callbackUrlScheme);
+      final code = Uri.parse(result).queryParameters['code'];
+      print(code);
+    } on PlatformException catch (e) {
+      setState(() {
+        _status = 'Got error: $e';
+      });
+    }
+  }
+
+  Future<void> get_request() async {
+    var url = Uri.parse(
+        "http://913d-2401-4900-5033-eb92-1d68-513b-3715-5571.ngrok.io/auth/o/google-oauth2/?redirect_uri=http://913d-2401-4900-5033-eb92-1d68-513b-3715-5571.ngrok.io");
+    var response = await http.get(url);
+    Map data = jsonDecode(response.body);
+    print(data["authorization_url"]);
+    authenticate(data["authorization_url"]);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initilization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print('ERROR');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        return MaterialApp(
-          title: 'FireStore',
-          theme: ThemeData(
-            textTheme:
-                GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
-            primarySwatch: Colors.blue,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Web Auth example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 80),
+              ElevatedButton(
+                child: const Text('Authenticate'),
+                onPressed: () {
+                  get_request();
+                },
+              ),
+            ],
           ),
-          debugShowCheckedModeBanner: false,
-          routes: {
-            "/home": (context) => HomePage(),
-            "/store": (context) => VisitStore(),
-          },
-          home: LoginPage(),
-        );
-      },
+        ),
+      ),
     );
   }
 }
